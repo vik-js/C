e62ccd23b5f2a539070a88685c4efe0766d44ce8
viktoriya <emillia@bk.ru>
2023-06-25 01:50:50 +0500

    Создание папки HomeWork06 и решение задачи 6

---
+++ HomeWork06/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+int evenNumber = a % 2;
+if (evenNumber == 0)
+{
+  Console.WriteLine($"Число является чётным? {a} -> да");
+}
+else
+{
+  Console.WriteLine($"Число является чётным? {a} -> нет");
+}
