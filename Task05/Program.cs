2de1062b550a1b61b498d3a3e1c5c4acad2b305a
viktoriya <emillia@bk.ru>
2023-06-24 16:14:44 +0500

    Создание папки Task05 и решение задачи 5

---
+++ Task05/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число N:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count = -number;
+while (count <= number)
+{
+  Console.Write($"{count} ");
+  count++;
+
+}
+
+
+
+// 5. Напишите программу, которая на вход принимает
+// одно число (N), а на выходе показывает все целые
+// числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> " -2, -1, 0, 1, 2"
