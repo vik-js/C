a658fb2ed63fa0e05dbf696bcdf7bc320deda7cd
viktoriya <emillia@bk.ru>
2023-06-25 00:55:09 +0500

    Создание папки HomeWork02 и решение задачи 2

---
+++ HomeWork02/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число a:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b:");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+  Console.WriteLine($"a = {a}; b = {b} -> max = {a}");
+}
+else
+{
+  Console.WriteLine($"a = {a}; b = {b} -> max = {b}");
+}
