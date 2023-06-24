eb519249e8e7f9a64b4489b9041a903a7a387f95
viktoriya <emillia@bk.ru>
2023-06-25 01:36:14 +0500

    Создание папки HomeWork04 и решение задачи 4

---
+++ HomeWork04/Program.cs
@@ -0,0 +1,56 @@
+﻿Console.WriteLine("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число:");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (max < b)
+{
+  max = b;
+  if (max < c)
+  {
+    max = c;
+    Console.WriteLine($"{a}, {b}, {c} -> {max}");
+  }
+  else
+  {
+    Console.WriteLine($"{a}, {b}, {c} -> {max}");
+  }
+}
+else if (max < c)
+{
+  max = c;
+  Console.WriteLine($"{a}, {b}, {c} -> {max}");
+}
+else
+{
+  Console.WriteLine($"{a}, {b}, {c} -> {max}");
+}
+
+
+// if (max > b)
+// {
+//   if (max > c)
+//   {
+//     Console.WriteLine($"{a}, {b}, {c} -> {max}");
+//   }
+//   else
+//   {
+//     max = c;
+//     Console.WriteLine($"{a}, {b}, {c} -> {max}");
+//   }
+// }
+// else
+// {
+//   max = b;
+//   if (max > c)
+//   {
+//     Console.WriteLine($"{a}, {b}, {c} -> {max}");
+//   }
+//   else
+//   {
+//     max = c;
+//     Console.WriteLine($"{a}, {b}, {c} -> {max}");
+//   }
+// }
