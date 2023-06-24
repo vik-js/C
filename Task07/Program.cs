401e9c2cf85eacc80531fc72ae42d16cfd88f1ed
viktoriya <emillia@bk.ru>
2023-06-24 16:16:55 +0500

    Создание папки Task07 и решение задачи 7

---
+++ Task07/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите 3-х значное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0)
+{
+  number *= -1;
+}
+if (number >= 100 && number <= 999)
+{
+  int lastNumber = number % 10;
+  Console.WriteLine($"Последняя цифра числа {number}: {lastNumber}");
+}
+else
+{
+  Console.WriteLine("Введено неверное число!");
+}
+
+
+// 7. Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает последнюю цифру
+// этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
