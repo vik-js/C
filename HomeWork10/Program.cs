8339b54e2f2d3d4825d69705712a97c6ea2745ab
viktoriya <emillia@bk.ru>
2023-06-25 14:07:14 +0500

    Создание папки HomeWork10 и решение задачи 10

---
+++ HomeWork10/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 10: Напишите программу, которая принимает на
+// вход трёхзначное число и на выходе показывает вторую
+// цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите 3-х значное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0)
+{
+  number *= -1;
+}
+if (number >= 100 && number <= 999)
+{
+  int secondDigit = number / 10 % 10;
+  Console.WriteLine($"Вторая цифра 3-х значного числа {number} -> {secondDigit}");
+}
+else
+{
+  Console.WriteLine("Введено НЕ 3-х значное число!");
+}
