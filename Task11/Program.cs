e0e61f075e403b379171091eb3fe421adbc3d408
viktoriya <emillia@bk.ru>
2023-06-25 14:05:14 +0500

    Создание папки Task11 и решение задачи 11

---
+++ Task11/Program.cs
@@ -0,0 +1,17 @@
+﻿int number = new Random().Next(100, 1000); // 999 + 1
+
+int firstDigit = number / 100;
+int lastDigit = number % 10; 
+int result = firstDigit * 10 + lastDigit;
+
+Console.WriteLine($"Случайное число от 100 - 999 -> {number}");
+Console.WriteLine($"Итоговое число без второй цифры -> {result}");
+
+
+
+// 11. Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
