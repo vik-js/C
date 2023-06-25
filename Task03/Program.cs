b8849f0ecea1dd08b5fdf2c1ac2d554730cf5497
viktoriya <emillia@bk.ru>
2023-06-25 11:51:36 +0500

    Создание папки Task03 и решение задачи 3

---
+++ Task03/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Введите номер дня недели:");
+string dayOfWeek = Console.ReadLine();
+
+if (dayOfWeek == "1")
+{
+  Console.WriteLine($"{dayOfWeek} -> Понедельник");
+}
+else if (dayOfWeek == "2")
+{
+  Console.WriteLine($"{dayOfWeek} -> Вторник");
+}
+else if (dayOfWeek == "3")
+{
+  Console.WriteLine($"{dayOfWeek} -> Среда");
+}
+else if (dayOfWeek == "4")
+{
+  Console.WriteLine($"{dayOfWeek} -> Четверг");
+}
+else if (dayOfWeek == "5")
+{
+  Console.WriteLine($"{dayOfWeek} -> Пятница");
+}
+else if (dayOfWeek == "6")
+{
+  Console.WriteLine($"{dayOfWeek} -> Суббота");
+}
+else if (dayOfWeek == "7")
+{
+  Console.WriteLine($"{dayOfWeek} -> Воскресенье");
+}
+else
+{
+  Console.WriteLine("В неделе только 7 дней =), введите число от 1 до 7");
+}
