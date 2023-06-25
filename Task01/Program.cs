42c7a570713d24f5a2cf0b7785d30fb057d9131c
viktoriya <emillia@bk.ru>
2023-06-25 11:38:07 +0500

    Создание папки Task01 и решение задачи 1

---
+++ Task01/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 == number2 * number2)
+{
+  Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+  Console.WriteLine("Первое число НЕ является квадратом второго");
+}
