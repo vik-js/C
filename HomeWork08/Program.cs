61fa0dba78e82a192f8168240f8a1571ad7b5aa3
viktoriya <emillia@bk.ru>
2023-06-30 18:11:40 +0500

    пересохранение задачи 8

--- HomeWork08/Program.cs
+++ HomeWork08/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+if (number > 0)
+{
+  while (count <= number)
+  {
+    if (count % 2 == 0)
+    {
+      if (count <= 2)
+      {
+        Console.Write($"{number} -> ");
+      }
+      if (count == number || (count == number - 1))
+      {
+        Console.Write($"{count}");
+      }
+      else
+      {
+        Console.Write($"{count},");
+      }
+    }
+    count++;
+  }
+}
+else
+{
+  Console.Write("Введите положительное число!");
+}
