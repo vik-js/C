b8def644e5b5a3d92591ca898892cd58ba9de059
viktoriya <emillia@bk.ru>
2023-07-01 22:17:16 +0500

    Создание папки HomeWork23 и решение задачи 23

---
+++ HomeWork23/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+Console.WriteLine("Введите положительное целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0)
+{
+  TableSquare(number);
+}
+else
+{
+  Console.WriteLine("Ошибка ввода!");
+}
+
+void TableSquare(int num)
+{
+  int count = 1;
+
+  while (count <= num)
+  {
+    Console.WriteLine($"{count} | {count * count * count}");
+    count++;
+  }
+}
