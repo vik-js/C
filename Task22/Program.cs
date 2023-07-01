7cda30af557fcc58de57d5c7ebe3c9d261b9d01a
viktoriya <emillia@bk.ru>
2023-07-01 20:30:26 +0500

    Создание папки Task22 и решение задачи 22

---
+++ Task22/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 22: Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+
+Console.WriteLine("Введите положительное целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0)
+{
+  TableSquare(number);
+}
+else
+{
+  Console.WriteLine("Ошибка ввода!");
+}
+
+void TableSquare(int num)
+{
+  int count = 1;
+
+  while (count <= num)
+  {
+    Console.WriteLine($"{count,3}  {count * count,5}");
+    count++;
+  }
+}
