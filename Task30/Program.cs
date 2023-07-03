5abef0668d91e6b10121b8348de0ab28a9765407
viktoriya <emillia@bk.ru>
2023-07-03 09:50:59 +0500

    Создание папки Task30 и решение задачи 30

---
+++ Task30/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 30: Напишите программу, которая
+// выводит массив из 8 элементов, заполненный
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+Console.WriteLine("Введите целое положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[number];
+FillArray(array);
+PrintArray(array);
+
+void FillArray(int[] arr)
+{
+  Random rnd = new Random();
+  for (int i = 0; i < arr.Length; i++)
+  {
+    arr[i] = rnd.Next(2);
+  }
+}
+
+void PrintArray(int[] arr)
+{
+  for (int i = 0; i < arr.Length; i++)
+  {
+    Console.Write($"{arr[i]} ");
+  }
+  Console.WriteLine();
+}
