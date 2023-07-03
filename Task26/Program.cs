1b0ef4d78f74dca889b96a34b729f9cabda7f34e
viktoriya <emillia@bk.ru>
2023-07-03 09:50:29 +0500

    Создание папки Task26 и решение задачи 26

---
+++ Task26/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.WriteLine("Введите целое число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int lengthNumbers = LengthNumber(number);
+Console.WriteLine($"{number} -> {lengthNumbers}");
+
+int LengthNumber(int num)
+{
+  int count = 0;
+  while (num != 0)
+  {
+    num /= 10;
+    count++;
+  }
+  return count == 0 ? 1 : count;
+}
