671e0be4edcc98b67835f5d2d7c70ec61c3c2e22
viktoriya <emillia@bk.ru>
2023-07-08 19:52:38 +0500

    Создание папки Task32 и решение задачи 32

---
+++ Task32/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 32: Напишите программу замена элементов
+// массива: положительные элементы замените на
+// соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+  int[] arr = new int[size];
+  Random rnd = new Random();
+
+  for (int i = 0; i < arr.Length; i++)
+  {
+    arr[i] = rnd.Next(min, max + 1);
+  }
+  return arr;
+}
+
+void PrintArray(int[] arr, string sep = ", ")
+{
+  for (int i = 0; i < arr.Length; i++)
+  {
+    if (i < arr.Length - 1) Console.Write($"{arr[i]}{sep}");
+    else Console.Write($"{arr[i]}");
+  }
+}
+
+
+void InverseArray(int[] arr)
+{
+  for (int i = 0; i < arr.Length; i++)
+  {
+    arr[i] *= -1;
+  }
+}
+
+int[] array = CreateArrayRndInt(12, -9, 9);
+Console.Write("[");
+PrintArray(array);
+Console.Write("]");
+Console.WriteLine();
+
+InverseArray(array);
+Console.Write("[");
+PrintArray(array);
+Console.Write("]");
