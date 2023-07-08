299dfd31e7190e5e2d6b981a0d866484f6f8b7b1
viktoriya <emillia@bk.ru>
2023-07-08 20:19:10 +0500

    Создание папки HomeWork34 и решение задачи 34

---
+++ HomeWork34/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 34: Задайте массив, заполненный случайными 
+// положительными трёхзначными числами. Напишите программу, 
+// которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+  int[] arr = new int[size];
+  Random rnd = new Random();
+
+  for (int i = 0; i < arr.Length; i++)
+  {
+    arr[i] = rnd.Next(min, max + 1);
+  }
+  return arr;
+}
+
+void PrintArray(int[] arr, string sep = ", ")
+{
+  for (int i = 0; i < arr.Length; i++)
+  {
+    if (i < arr.Length - 1) Console.Write($"{arr[i]}{sep}");
+    else Console.Write($"{arr[i]}");
+  }
+}
+
+int CountPositiveEvenNumbers(int[] arr)
+{
+  int count = 0;
+  for (int i = 0; i < arr.Length; i++)
+  {
+    if (arr[i] % 2 == 0)
+    {
+      count++;
+    }
+  }
+  return count;
+}
+
+int[] array = CreateArrayRndInt(10, 100, 999);
+Console.Write("[");
+PrintArray(array);
+Console.Write("]");
+Console.WriteLine();
+
+int countPositiveEvenNumbers = CountPositiveEvenNumbers(array);
+Console.Write($"Количество положительных четных чисел: {countPositiveEvenNumbers}");
+Console.WriteLine();
