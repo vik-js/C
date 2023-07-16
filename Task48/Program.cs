7fc94d49932290dc9ae6ab9b231fadd6ffe1aa4c
viktoriya <emillia@bk.ru>
2023-07-16 21:09:02 +0500

    Создание папки Task48 и решение задачи 48

---
+++ Task48/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n,
+// каждый элемент в массиве находится по формуле: Amn = m+n.
+// Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+int[,] CreateMatrixSumInd(int rows, int columns)
+{
+  int[,] matrix = new int[rows, columns];
+
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      matrix[i, j] = i + j;
+    }
+  }
+  return matrix;
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    Console.Write("|");
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      Console.Write($"{matrix[i, j],5} ");
+    }
+    Console.WriteLine("|");
+  }
+}
+
+int[,] array2d = CreateMatrixSumInd(4, 5);
+PrintMatrix(array2d);
