526fbefc70a99c39aea17e1a2daeb6241a6370c5
viktoriya <emillia@bk.ru>
2023-07-17 05:10:06 +0500

    Создание папки HomeWork52 и решение задачи 52

---
+++ HomeWork52/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задача 52: Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+  int[,] matrix = new int[rows, columns];
+
+  Random rnd = new Random();
+
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      matrix[i, j] = rnd.Next(min, max + 1);
+    }
+  }
+  return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    Console.Write("|");
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      Console.Write($"{matrix[i, j],3} ");
+    }
+    Console.WriteLine("|");
+  }
+}
+
+double[] FindArithmeticMean(int[,] matrix)
+{
+  double[] array = new double[matrix.GetLength(1)];
+  double sum = 0;
+  for (int j = 0; j < matrix.GetLength(1); j++)
+  {
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+      sum = sum + matrix[i, j];
+    }
+    sum = Math.Round(sum / (matrix.GetLength(1) + 1), 1);
+    array[j] = sum;
+    sum = 0;
+  }
+  return array;
+}
+
+void PrintArray(double[] arr, string sep = "; ")
+{
+  Console.Write($"Среднее арифметическое каждого столбца: ");
+  for (int i = 0; i < arr.Length; i++)
+  {
+    if (i < arr.Length - 1) Console.Write($"{arr[i]}{sep}");
+    else Console.WriteLine($"{arr[i]}");
+  }
+}
+
+int[,] matrix2d = CreateMatrixRndInt(4, 3, 1, 9);
+PrintMatrix(matrix2d);
+Console.WriteLine();
+
+double[] findArithmeticMean = FindArithmeticMean(matrix2d);
+PrintArray(findArithmeticMean);
