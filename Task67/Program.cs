a926119520418ce337c3e1f6dd2daa94d9f04925
viktoriya <emillia@bk.ru>
2023-07-22 19:33:29 +0500

    Создание папки HomeWork67 и решение задачи 67

---
+++ Task67/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.WriteLine("Введите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumDigits(int num)
+{
+  if (num == 0) return 0;
+  return num % 10 + SumDigits(num / 10);
+}
+
+Console.WriteLine($"{number} -> {SumDigits(number)}");
+
+// int FactorialRec(int n)
+// {
+//   if (n == 1) return 1;
+//   else return n * FactorialRec(n - 1);
+// }
+
+// Console.WriteLine(FactorialRec(10)); // 3628800
